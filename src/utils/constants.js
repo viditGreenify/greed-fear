3b40431478e89d8ae6e2b05ca2b8ceeacfea58de
Vidit Bansal <viditban@gmail.com>
2025-01-27 17:04:26 +0530

    misc

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,13 @@
 const constants = {
   niftyFutureDailyTicker: 'https://www.nseindia.com/api/liveEquity-derivatives?index=nse50_fut', // create real time chart for nifty
   spotNifty: 'https://www.nseindia.com/api/marketStatus',
-  stockFuture: 'https://www.nseindia.com/api/option-chain-equities?symbol=',
+  stockFuture: 'https://www.nseindia.com/api/option-chain-equities?symbol=SBIN',
   dowFutDataUrl:
     'https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?symbols=%40DJ.1&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1',
   SGXNifty:
     'https://priceapi.moneycontrol.com/technicalCompanyData/globalMarket/getGlobalIndicesListingData?view=overview&deviceType=W',
   stochasticDataURL: 'https://www.nseindia.com/api/marketStatus',
-  niftyOIURL: 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY',
+  niftyOIURL: 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', //'https://www.nseindia.com/api/option-chain-equities?symbol=HINDUNILVR', //'https://www.nseindia.com/api/option-chain-indices?symbol=SBIN',
 };
 
 module.exports = { constants };
--- src/utils/fetchRequest.js
+++ src/utils/fetchRequest.js
@@ -2,19 +2,35 @@ const axios = require('axios');
 const logger = require('../config/logger');
 
 const getDataFromNSE = async (dataURL) => {
-  return axios
-    .get(dataURL)
-    .then(async (response) => {
-      // handle success
-      const { records } = response.data;
-      // console.log(records)
-      return records;
-    })
-    .catch((error) => {
-      // handle error
-      logger.error(error);
-      throw error;
+  try {
+    const response = await axios.get(dataURL, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+        'Accept': 'application/json', // Assuming JSON response
+        'Referer': 'https://www.nseindia.com/option-chain',
+        'authority':'www.nseindia.com',
+      }
     });
+    return response.data.records; // Assuming 'records' is always present
+  } catch (error) {
+    logger.error('Error fetching data from NSE:', error);
+    throw error; // Re-throw the error for further handling
+  }
+
+//   axios.get(dataURL, {
+//   headers: {
+//     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+//     'Accept': 'application/json' // Assuming JSON response
+//   }
+// })
+// .then(response => {
+//   console.log(response.data);
+//   return response.data.records
+// })
+// .catch(error => {
+//     logger.error('Error fetching data from NSE:', error);
+//     throw error; // Re-throw the error for further handling
+// });
 };
 
 module.exports = { getDataFromNSE };
